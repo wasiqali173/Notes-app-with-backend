715c5e738502205bc382c7b394c1dd1e14753b80
Wasiq Ali <wasiqali173@gmail.com>
2025-11-13 20:22:10 +0500

    add frontend url

--- Backend/index.js
+++ Backend/index.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: 'http://localhost:5173', 
+    origin: 'https://notes-app-with-backend-plgg.vercel.app', 
     credentials: true,               
   })
 );
